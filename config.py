8374af8884ca80438723ce00543e89a9136036fa
lamwaihong123 <101498426+lamwaihong123@users.noreply.github.com>
2023-09-08 18:58:20 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c3grv0r1ywua.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lamwaihong-employee"
+customregion = "us-east-1"
+
